8e4dfb21f75ee0b610ad1976ed638644927240b6
Danila <novash.danila@yandex.by>
2025-06-01 22:51:10 +0300

    Version 0.1 The first draft of the documentation has been prepared.

---
+++ src/types/index.ts
@@ -0,0 +1,60 @@
+export interface ICard {
+  category: string;
+  title: string;
+  image: string;
+  price: number | null;
+  description?: string;
+  id: string;
+}
+
+export interface IOrder {
+  total: number;
+  items: string[];
+  payment: string;
+  address: string;
+  mail: string;
+  phone: string;
+}
+
+export interface ICardsData {
+  cards: ICard[];
+  preview: string | null;
+  addCards(card: ICard[]): void;
+  findCard(cardId: string): ICard | undefined;
+  previewCard(cardId: string): void;
+  getCards(cardId: string): ICard;
+}
+
+export interface ICartData {
+  items: ICard[];
+  total: number | null;
+  count: number;
+  addToCart(card: ICard): void;
+  removeFromCart(cardId: string): void;
+  countPrices(): void;
+  countCardsAmount(): void;
+}
+
+export interface IOrderData {
+  payment: string;
+  address: string;
+  mail: string;
+  phone: string;
+  createOrder(order: IOrder): void;
+  setPayment(payment: string): void;
+  setAddress(address: string): void;
+  setMail(mail: string): void;
+  setPhone(phone: string): void;
+  submitOrder(): void;
+  validateOrderFields(data: Record<keyof TPaymentModal & TContactModal, string>): boolean;
+}
+
+export type TMainPage = Pick<ICard, 'category' | 'title' | 'image' | 'price' | 'id'>;
+
+export type TCardModal = Pick<ICard, 'category' | 'title' | 'image' | 'price' | 'description' | 'id'>;
+
+export type TCartModal = Pick<ICard, 'title' | 'price' | 'id'> & Pick<IOrder, 'total' | 'items'>;
+
+export type TPaymentModal = Pick<IOrder, 'payment' | 'address'>;
+
+export type TContactModal = Pick<IOrder, 'mail' | 'phone'>;
