9950a685cbeb6b34a1cf361dd694f49b3de92c7a
Danila <novash.danila@yandex.by>
2025-06-07 18:57:56 +0300

    Version 1.7 CartData class implemented

---
+++ src/components/Model/CartData.ts
@@ -0,0 +1,44 @@
+import { ICartData, TCartModal } from "../../types";
+import { IEvents } from "../base/Events";
+
+export class CartData implements ICartData {
+    protected _items: TCartModal[];
+    protected _total: number | null;
+    protected _count: number;
+    protected events: IEvents;
+
+    constructor(events: IEvents) {
+      this.events = events;
+    }
+
+    addToCart(item: TCartModal) {
+      this._items.push(item);
+      this.events.emit('card:added');
+    };
+
+    removeFromCart(cardId: string) {
+      this._items = this._items.filter((item) => item.id !== cardId);
+      this.events.emit('card:removed');
+    }
+
+    countPrices() {
+      this._total = this._items.reduce((sum, item) => sum + (item.price || 0), 0);
+      this.events.emit('cart:changed');
+    }
+
+    countCardsAmount() {
+      this._count = this._items.length;
+    }
+
+    get items() {
+      return this._items;
+    }
+
+    get total() {
+      return this._total;
+    }
+
+    get count() {
+      return this._count;
+    }
+}
