fdc4054c03e026d99763bd3d5f3ed98b176c916d
Danila <novash.danila@yandex.by>
2025-06-09 17:21:26 +0300

    Version 1.11 Cart implemented

---
+++ src/components/View/Cart.ts
@@ -0,0 +1,41 @@
+import { ICartData } from "../../types";
+import { createElement, ensureElement } from "../../utils/utils";
+import { Component } from "../base/Component";
+
+export class Cart extends Component<ICartData> {
+  protected _cartList: HTMLElement;
+  protected _cartTotal: HTMLElement;
+  protected _cartButton: HTMLElement;
+
+  constructor(container: HTMLElement) {
+    super(container);
+
+    this._cartList = ensureElement('.basket__list', this.container);
+    this._cartTotal = ensureElement('.basket__price', this.container);
+    this._cartButton = ensureElement('basket__button', this.container);
+    
+    this.items = []
+  }
+
+  set items(items: HTMLElement[]) {
+    if (items.length) {
+      this._cartList.replaceChildren(...items);
+    } else {
+      this._cartList.replaceChildren(createElement<HTMLParagraphElement>('p', {
+        textContent: 'Корзина пуста'
+      }))
+    }
+  }
+
+  set selected(items: string[]) {
+    if (items.length) {
+      this.setDisabled(this._cartButton, false);
+    } else {
+      this.setDisabled(this._cartButton, true);
+    }
+  }
+
+  set total(value: string) {
+    this.setText(this._cartTotal, `$(value) синапсов`);
+  }
+}
