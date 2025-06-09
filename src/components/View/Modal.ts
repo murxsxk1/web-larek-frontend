7213b4607ab737734ebf24925989fb98033e35be
Danila <novash.danila@yandex.by>
2025-06-09 18:09:23 +0300

    Version 1.13 Modal implemented

---
+++ src/components/View/Modal.ts
@@ -0,0 +1,42 @@
+import { IModalData } from "../../types";
+import { ensureElement } from "../../utils/utils";
+import { Component } from "../base/Component";
+import { IEvents } from "../base/Events";
+
+export class Modal extends Component<IModalData> {
+  protected _modalCloseButton: HTMLButtonElement;
+  protected _modalContent: HTMLElement;
+
+  constructor(container: HTMLElement, events: IEvents) {
+    super(container);
+
+    this._modalCloseButton = ensureElement<HTMLButtonElement>('.modal__close', this.container);
+    this._modalContent = ensureElement<HTMLElement>('.modal__content', this.container);
+
+    this._modalCloseButton.addEventListener('click', this.close.bind(this));
+    this.container.addEventListener('click', this.close.bind(this));
+    this._modalContent.addEventListener('click', (event) => event.stopPropagation());
+  }
+
+  set content(value: HTMLElement) {
+    this._modalCloseButton.replaceChildren(value);
+  }
+
+  open() {
+    this.container.classList.add('.modal_active');
+    this.events.emit('modal:open');
+  }
+
+  close() {
+    this.container.classList.remove('.modal_active');
+    this.content = null;
+    this.events.emit('modal:close');
+  }
+
+  render(data: IModalData): HTMLElement {
+    super.render(data);
+    this.open();
+    return this.container;
+  }
+
+}
